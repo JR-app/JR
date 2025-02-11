3c9f1031901687d99e1dfe2b04151cc9c3caa859
tswu <tswu@umich.edu>
2025-02-11 01:04:29 -0500

    chore(api): api http request boilerplate

--- api/main.go
+++ api/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "root address\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello World!\n")
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+
+	err := http.ListenAndServe(":3333", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("ERROR: server closed\n")
+	} else if err != nil {
+		fmt.Printf("ERROR: server start error")
+		os.Exit(1)
+	}
 }
