0b83832f25fb72be298da9684d7e34b6a9d05f99
tswu <tswu@umich.edu>
2025-02-12 04:30:17 -0500

    halfway done with oauth

--- JR/app/(tabs)/index.tsx
+++ JR/app/(tabs)/index.tsx
@@ -2,11 +2,13 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import GithubAuthButton from '@/components/GithubAuthButton';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Jerkmate Ranked</Text>
+      <GithubAuthButton/>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
