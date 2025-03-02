fd9c3d440a9d852180cbf47885db65aa793ebfe1
Tongsheng Wu <tswu@Tongshengs-MacBook-Air.local>
2025-03-02 01:00:52 -0500

    chore: created ios swift files, moved react native files

---
+++ jr-ios/jr-ios/jr_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  jr_iosApp.swift
+//  jr-ios
+//
+//  Created by Tongsheng Wu on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct jr_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ jr-ios/jr-iosTests/jr_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  jr_iosTests.swift
+//  jr-iosTests
+//
+//  Created by Tongsheng Wu on 3/2/25.
+//
+
+import Testing
+@testable import jr_ios
+
+struct jr_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
